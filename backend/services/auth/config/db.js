9fdb7e79f99cea8d7dab65f3a246ba8537bb0f30
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-27 23:39:07 +0530

    some new chnages for db

---
+++ backend/services/auth/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
+
+export const connectdb = async (req , res)=>{
+    try {
+        await mongoose.connect(process.env.MONGOOSE_URL)
+    } catch (error) {
+        
+    }
+}
