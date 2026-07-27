a93301bd206f11ab6197a94e24382416b308319c
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-27 23:31:49 +0530

    gateway created and working properly

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import dotenv from "dotenv"
 dotenv.config()
+import proxy from "express-http-proxy";
 
 const port = process.env.PORT || 8000
 const app = express()
+
+app.use(express.json())
+
+app.use("/api/auth" , proxy((process.env.AUTH_URL)))
+
+app.get("/",(req , res)=>{
+    res.status(200).json("Gateway server running successfully")
+})
 
 app.listen(port , (req , res)=>{
     console.log("Gateway Server is listening on port: ",port)
--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -5,6 +5,10 @@ dotenv.config()
 const port = process.env.PORT || 8001
 const app = express()
 
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Auth server is running successfully"})
+})
+
 app.listen(port , (req , res)=>{
     console.log("Auth Server is listening on port: ",port)
 })
