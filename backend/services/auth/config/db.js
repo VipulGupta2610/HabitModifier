1ca53542e836ac88bd24b0702ed258e6d545b23c
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-29 23:30:26 +0530

    scope of error identified and fixed

--- backend/services/auth/config/db.js
+++ backend/services/auth/config/db.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+
 dotenv.config()
 
 export const connectdb = async (req , res)=>{
     try {
-        await mongoose.connect(process.env.MONGOOSE_URL)
+        await mongoose.connect(process.env.MONGOOSE_URL);
+        console.log("Database connected")
     } catch (error) {
-        
+        console.log("Error at connectdb");
+        console.log(error)
     }
 }
