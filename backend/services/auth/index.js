971b65089ee8fd22756fa3b6e918602795fac5e4
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-31 15:08:23 +0530

    prepring for session based authentication

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv"
+import session from "express-session";
 dotenv.config()
 
 const port = process.env.PORT || 8001
 const app = express()
+
+app.use(express.json())
+
+app.use(session())
 
 app.get("/",(req,res)=>{
     res.status(200).json({message:"Auth server is running successfully"})
